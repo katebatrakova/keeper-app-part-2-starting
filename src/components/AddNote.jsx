2fc39ee044d14efa4bc2eba4fdccdf1c56100062
katebatrakova <katerynabatrakova@gmail.com>
2020-11-02 12:04:16 -0500

    Implement a Delete Note functionality

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 
 export const AddNote = (props) => {
   //creating constant that keeps track of the note title and content
-  const [note, setNote] = useState([
+  const [note, setNote] = useState(
     {
-      title: 'NOTE 1',
-      content: 'This is my first note'
-    }])
+      title: '',
+      content: ''
+    })
 
   const handleChange = (event) => {
     const { name, value } = event.target
@@ -24,6 +24,13 @@ export const AddNote = (props) => {
   const submitNote = (event) => {
     //pass over the current created note object to be used inside App
     props.onAdd(note)
+
+    //clear the title and content after submitting
+    setNote({
+      title: "",
+      content: ""
+    })
+
     //prevent reloading of the page
     event.preventDefault()
   }
@@ -34,10 +41,10 @@ export const AddNote = (props) => {
     <div className='add-note'>
       <form>
         <div>
-          <input onChange={handleChange} name='title' placeholder='Add a title ...'></input>
+          <input value={note.title} onChange={handleChange} name='title' placeholder='Add a title ...'></input>
         </div>
         <div>
-          <input onChange={handleChange} name='content' placeholder='Add content ...'></input>
+          <input value={note.content} onChange={handleChange} name='content' placeholder='Add content ...'></input>
         </div>
         <button onClick={submitNote}>Add a new note</button>
       </form>
--- src/components/App.js
+++ src/components/App.js
@@ -21,20 +21,34 @@ function App() {
 
   }
 
+
+  const deleteNote = (id) => {
+
+    setNotes(prevNotes => {
+      // loop trough previos notes
+      return prevNotes.filter((noteItem, index) => {
+        // return all the note where the index doesn't equal the id od the deleted item
+        return index !== id;
+      })
+    })
+
+  }
+
   return (
     <div>
       <Header />
       <AddNote
         onAdd={addNote}
-
       />
-      {notes.map((singleNote) => {
+      {notes.map((singleNote, index) => {
 
         return (
           <Note
-            key={singleNote.id}
+            key={index}
+            id={index}
             title={singleNote.title}
             content={singleNote.content}
+            onDelete={deleteNote}
           />
         )
 
